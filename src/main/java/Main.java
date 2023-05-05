7c72877214631b0715b1d3cd6b72b275d62f95c3
a464442700 <464442700@qq.com>
2023-05-05 16:26:36 +0800

    使用registerBean向Spring上下文注册对象

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,30 @@
 import com.Parrot;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Main {
-    public static void main(String[] args) {
-      //  Parrot p = new Parrot();
-        var context =new AnnotationConfigApplicationContext(ProjectConfig.class);
-//        Parrot p2 = context.getBean("parrot2",Parrot.class);
-//        Parrot p1 = context.getBean("parrot",Parrot.class);
-//       System.out.println(p1.getName());
+import java.util.function.Supplier;
 
-        Parrot p1 = context.getBean(Parrot.class);
-        System.out.println(p1.getName());
+public class Main {
+    //    public static void main(String[] args) {
+//      //  Parrot p = new Parrot();
+//        var context =new AnnotationConfigApplicationContext(ProjectConfig.class);
+////        Parrot p2 = context.getBean("parrot2",Parrot.class);
+////        Parrot p1 = context.getBean("parrot",Parrot.class);
+////       System.out.println(p1.getName());
+//
+//        Parrot p1 = context.getBean(Parrot.class);
+//        System.out.println(p1.getName());
+//    }
+    public static void main(String[] args) {
+        var context =
+                new AnnotationConfigApplicationContext(
+                        ProjectConfig.class);
+        Parrot x = new Parrot();
+        x.setName("Kiki");
+        Supplier<Parrot> parrotSupplier = () -> x;
+        context.registerBean("parrot1",
+                Parrot.class, parrotSupplier);
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
     }
 }
