80f928bff6ec1614b225ba2563456a842ac19f4b
a464442700 <464442700@qq.com>
2023-06-08 17:24:17 +0800

    2023-06-08

--- src/main/java/aop/start1/ProjectConfiguration.java
+++ src/main/java/aop/start1/ProjectConfiguration.java
@@ -3,6 +3,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "aop.start2")
+@ComponentScan(basePackages = "aop.start1")
 public class ProjectConfiguration {
 }
--- src/main/java/aop/start2/Main.java
+++ src/main/java/aop/start2/Main.java
@@ -2,9 +2,13 @@ package aop.start2;
 
 import aop.start2.pojo.Comment;
 import aop.start2.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
